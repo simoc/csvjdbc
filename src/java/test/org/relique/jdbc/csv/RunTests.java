dc21f4b2859e1af5edeccc9fa2bc3dd3235d088e
gupta_chetan <gupta_chetan>
2005-05-13 02:22:00 +0000

    Just added copyright Information

--- src/java/test/org/relique/jdbc/csv/RunTests.java
+++ src/java/test/org/relique/jdbc/csv/RunTests.java
@@ -1,10 +1,33 @@
+/*
+CsvJdbc - a JDBC driver for CSV files
+Copyright (C) 2001  Jonathan Ackerman
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 package test.org.relique.jdbc.csv;
 
 import junit.framework.*;
 
+/**This class is used to test the CsvJdbc driver.
+*
+* @author Jonathan Ackerman
+* @version $Id: RunTests.java
+*/
 public class RunTests
 {
-  public static String DEFAULT_FILEPATH="C:\\Downloads\\opensource\\sourceforge\\csvjdbc\\csvjdbc-r0-9\\src\\src\\testdata";
+  public static String DEFAULT_FILEPATH="C:\\Chetan\\Technical\\java\\csvjdbc\\src\\testdata";
   
   public static Test suite()
   {
