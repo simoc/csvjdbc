1a1c0bb1d486bd50f55cc49fa27d37bafe616e02
mfrasca <mfrasca>
2010-01-14 14:10:52 +0000

    made date format a bit more flexible. (just a few more tests).

--- src/java/test/org/relique/jdbc/csv/TestStringConverter.java
+++ src/java/test/org/relique/jdbc/csv/TestStringConverter.java
@@ -36,4 +36,32 @@ public class TestStringConverter extends TestCase {
 		assertEquals(got, expect);
 	}
 
+	public void testParseDateVariableSizeYMD() {
+		StringConverter sc = new StringConverter("yyyy-m-d","");
+
+		Date got, expect;
+
+		got = sc.parseDate("1980-12-01");
+		expect = java.sql.Date.valueOf("1980-12-01");
+		assertEquals(got, expect);
+		
+		got = sc.parseDate("1983-3-9");
+		expect = java.sql.Date.valueOf("1983-03-09");
+		assertEquals(got, expect);
+	}
+
+	public void testParseDateVariableSizeMYD() {
+		StringConverter sc = new StringConverter("m-yyyy-d","");
+
+		Date got, expect;
+
+		got = sc.parseDate("12-1980-01");
+		expect = java.sql.Date.valueOf("1980-12-01");
+		assertEquals(got, expect);
+		
+		got = sc.parseDate("3-1983-9");
+		expect = java.sql.Date.valueOf("1983-03-09");
+		assertEquals(got, expect);
+	}
+
 }
