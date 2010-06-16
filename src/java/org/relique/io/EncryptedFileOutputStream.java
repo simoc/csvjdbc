bb329eb5db1bacc7b1ef34cf632543abc771356c
mfrasca <mfrasca>
2010-06-16 06:42:20 +0000

    partially reverting previous change, removing unneeded method from utility class EncryptedFileOutputStream.

--- src/java/org/relique/io/EncryptedFileOutputStream.java
+++ src/java/org/relique/io/EncryptedFileOutputStream.java
@@ -23,10 +23,4 @@ public class EncryptedFileOutputStream extends OutputStream {
 			filter.write(out, b);
 	}
 
-	public void write(String string) throws IOException {
-		char[] chars = string.toCharArray();
-		for(int pos=0; pos<string.length(); pos++)
-			write(chars[pos]);
-	}
-
 }
