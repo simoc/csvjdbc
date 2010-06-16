7cab66595e61fed8c3e26445b165c95ab3ba2a98
mfrasca <mfrasca>
2010-06-16 06:47:28 +0000

    most current libraries, plus script to generate the tar.gz files. the script contains a local directory, correct where needed.

---
+++ pack-sources.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+JAR=../LizardAnt/dist/csvjdbc.jar
+SOURCES=$(find src/java/org/relique/ -iname "*.java")
+TESTSOURCES=src/java/test/org/relique/jdbc/csv/*.java
+TESTDATA=$(find src/testdata -maxdepth 1 -type f -not -name "*~")
+
+tar czf dist/csvjdbc-sources.tar.gz $SOURCES
+tar czf dist/csvjdbc-sources-full.tar.gz $SOURCES $TESTSOURCES $TESTDATA
+cp $JAR dist/.
