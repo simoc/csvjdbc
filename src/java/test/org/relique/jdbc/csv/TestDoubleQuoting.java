fe016d78eff75ef9b985c488a4067c4f068892f0
Simon Chenery <simoc@users.sourceforge.net>
2012-11-01 13:05:22 +0100

    Remove unused import.

--- src/java/test/org/relique/jdbc/csv/TestDoubleQuoting.java
+++ src/java/test/org/relique/jdbc/csv/TestDoubleQuoting.java
@@ -21,7 +21,6 @@ package test.org.relique.jdbc.csv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
