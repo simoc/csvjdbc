021a2ce0c580fba1ce7eb7dafe316bdf727b86d1
Simon Chenery <simoc@users.sourceforge.net>
2013-09-25 19:56:06 +0200

    Add license header, improve formatting

--- src/java/org/relique/jdbc/dbf/DbfReader.java
+++ src/java/org/relique/jdbc/dbf/DbfReader.java
@@ -1,3 +1,18 @@
+/*
+ *  CsvJdbc - a JDBC driver for CSV files
+ *  Copyright (C) 2011  Mario Frasca
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package org.relique.jdbc.dbf;
 
 import java.io.File;
@@ -10,7 +25,8 @@ import java.util.Map;
 
 import org.relique.io.DataReader;
 
-public class DbfReader extends DataReader {
+public class DbfReader extends DataReader
+{
 	private Object table = null;
 	private List fields;
 	private Object record;
@@ -29,38 +45,46 @@ public class DbfReader extends DataReader {
 	private Map<String, String> dbfTypeToSQLType;
 	private String tableAlias;
 
-	public DbfReader(String path, String tableAlias) throws SQLException {
+	public DbfReader(String path, String tableAlias) throws SQLException
+	{
 		super();
-		try {
+		try
+		{
 			fieldClass = Class.forName("nl.knaw.dans.common.dbflib.Field");
 			recordClass = Class.forName("nl.knaw.dans.common.dbflib.Record");
 			tableClass = Class.forName("nl.knaw.dans.common.dbflib.Table");
-		} catch (ClassNotFoundException e) {
+		} 
+		catch (ClassNotFoundException e)
+		{
 			throw new SQLException("can't find the 'dans' library.");
 		}
-		try {
+		try
+		{
 			tableOpenMethod = tableClass.getMethod("open", new Class[] {});
 			tableCloseMethod = tableClass.getMethod("close", new Class[] {});
-			tableGetFieldsMethod = tableClass.getMethod("getFields",
-					new Class[] {});
+			tableGetFieldsMethod = tableClass.getMethod("getFields", new Class[] {});
 			fieldGetNameMethod = fieldClass.getMethod("getName", new Class[] {});
 			tableGetRecordAtMethod = tableClass.getMethod("getRecordAt", new Class[] {Integer.TYPE}); 
 			recordGetTypedValueMethod = recordClass.getMethod("getTypedValue", new Class[] {String.class});
 			fieldGetTypeMethod = fieldClass.getMethod("getType", new Class[] {});
 			fieldGetLengthMethod = fieldClass.getMethod("getLength", new Class[] {});
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			throw new SQLException("Error while being smart:" + e);
 		}
-		try {
+		try
+		{
 			Constructor<?> tableConstructor = tableClass.getConstructor(new Class[] {File.class});
-			table = tableConstructor
-					.newInstance(new Object[] { new File(path) });
+			table = tableConstructor.newInstance(new Object[] { new File(path) });
 			tableOpenMethod.invoke(table, new Object[] {});
 			fields = (List) tableGetFieldsMethod.invoke(table, new Object[] {});
 			record = null;
 			rowNo = -1;
 			this.tableAlias = tableAlias;
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			throw new SQLException("Error while being smart:" + e);
 		}
 		dbfTypeToSQLType = new HashMap<String, String>();
@@ -70,101 +94,137 @@ public class DbfReader extends DataReader {
 		dbfTypeToSQLType.put("DATE", "Date");
 	}
 
-	public void close() throws SQLException {
-		if(table != null)
-			try {
+	public void close() throws SQLException
+	{
+		if (table != null)
+		{
+			try
+			{
 				tableCloseMethod.invoke(table, new Object[] {});
-			} catch (Exception e) {
+			}
+			catch (Exception e)
+			{
 				throw new SQLException("Error while being smart:" + e);
 			}
+		}
 		table = null;
 	}
 
-	public String[] getColumnNames() throws SQLException {
+	public String[] getColumnNames() throws SQLException
+	{
 		int columnCount = fields.size();
 		String[] result = new String[columnCount];
-		for(int i=0; i < columnCount; i++) {
+		for(int i=0; i < columnCount; i++)
+		{
 			Object field = fields.get(i);
-			try {
+			try
+			{
 				result[i] = (String) fieldGetNameMethod.invoke(field, new Object[] {});
-			} catch (Exception e) {
+			}
+			catch (Exception e)
+			{
 				throw new SQLException("Error while being smart:" + e);
 			}
 		}
 		return result;
 	}
 
-	public Object getField(int i) throws SQLException {
+	public Object getField(int i) throws SQLException
+	{
 		Object field = fields.get(i - 1);
-		try {
+		try
+		{
 			String fieldName = (String) fieldGetNameMethod.invoke(field, new Object[] {});
 			Object result = recordGetTypedValueMethod.invoke(record, new Object[] {fieldName});
 			if(result instanceof String)
 				result = ((String) result).trim();
 			return result;
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			throw new SQLException("Error while being smart: " + e);
 		}
 	}
 
-	public boolean next() throws SQLException {
+	public boolean next() throws SQLException
+	{
 		rowNo++;
-		try {
+		try
+		{
 			record = tableGetRecordAtMethod.invoke(table, new Object[]{Integer.valueOf(rowNo)});
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			return false;
 		} 
 		return true;
 	}
 
-	public String[] getColumnTypes() throws SQLException {
+	public String[] getColumnTypes() throws SQLException
+	{
 		String[] result = new String[fields.size()];
-		for(int i=0; i<fields.size(); i++) {
-			try {
+		for(int i=0; i<fields.size(); i++)
+		{
+			try
+			{
 				String dbfType = fieldGetTypeMethod.invoke(fields.get(i), new Object[] {}).toString();
 				result[i] = dbfTypeToSQLType.get(dbfType);
-			} catch (Exception e) {
+			}
+			catch (Exception e)
+			{
 				throw new SQLException("Error while being smart:" + e);
 			}
 		}
 		return result;
 	}
 
-	public int[] getColumnSizes() throws SQLException {
+	public int[] getColumnSizes() throws SQLException
+	{
 		int[] result = new int[fields.size()];
-		for(int i=0; i<fields.size(); i++) {
-			try {
+		for(int i=0; i<fields.size(); i++)
+		{
+			try
+			{
 				Object fieldLength = fieldGetLengthMethod.invoke(fields.get(i), new Object[] {});
 				result[i] = ((Number)fieldLength).intValue();
-			} catch (Exception e) {
+			}
+			catch (Exception e)
+			{
 				throw new SQLException("Error while being smart:" + e);
 			}
 		}
 		return result;
 	}
 
-	public Map<String, Object> getEnvironment() throws SQLException {
+	public Map<String, Object> getEnvironment() throws SQLException
+	{
 		Map<String, Object> result = new HashMap<String, Object>();
-		for(int i=0; i < fields.size(); i++) {
+		for(int i=0; i < fields.size(); i++)
+		{
 			Object field = fields.get(i);
-			try {
+			try
+			{
 				String fieldName = (String) fieldGetNameMethod.invoke(field, new Object[] {});
 				Object o = getField(i + 1);
 				result.put(fieldName, o);
-				if (tableAlias != null) {
+				if (tableAlias != null)
+				{
 					/*
 					 * Also allow field value to be accessed as S.ID  if table alias S is set.
 					 */
 					result.put(tableAlias + "." + fieldName, o);
 				}
-			} catch (Exception e) {
+			}
+			catch (Exception e)
+			{
 				throw new SQLException("Error while being smart: " + e);
 			}
 		}
 		return result;
 	}
 
-	public String getTableAlias() {
+	public String getTableAlias()
+	{
 		return tableAlias;
 	}
 }
