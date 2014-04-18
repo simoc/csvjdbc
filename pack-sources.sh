bfc21f35e234557335291b0ba139a4abeae5218e
Simon Chenery <simoc@users.sourceforge.net>
2014-04-18 15:34:51 +0200

    Update paths for where.jj, resources, test classes

--- pack-sources.sh
+++ pack-sources.sh
@@ -2,8 +2,10 @@
 
 JAR=../LizardConverter/dist/csvjdbc.jar
 find src/main/java/org/relique/ -iname "*.java" > sources.txt
+find src/main/javacc/org/relique/ -iname "*.jj" >> sources.txt
+find src/main/resources -iname "*.properties" >> sources.txt
 cat sources.txt > sources-full.txt
-/bin/ls src/java/test/org/relique/jdbc/csv/*.java >> sources-full.txt
+/bin/ls src//test/java/org/relique/jdbc/csv/*.java >> sources-full.txt
 find src/testdata -maxdepth 1 -type f -not -name "*~" >> sources-full.txt
 
 tar czf dist/csvjdbc-sources.tar.gz -T sources.txt
